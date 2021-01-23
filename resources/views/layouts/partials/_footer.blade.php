aef8f214b748ce6d58804c20670228f0e824ee9c
ahmeth mbacke amar <ahmeth.amar@gmail.com>
2021-01-23 14:06:09 +0000

    Setup and static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="text-center">
+    <p>
+        &copy; {{ date('Y') }} &middot; Laracarte by @amardarmanko &dash; 
+        A <a href="https://laramap.com">laramap</a> clone app.
+    </p>
+    
+    <p>
+        <strong>This app has been built for learning purposes.</strong>
+    </p>
+</footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.default')
+    
+@section('content')
+    <div class="container">
+        <h2>What is Laracarte ?</h2>  
+        <p>Laracarte is a clone app of <a href="https://Laramap.com" target="_blank">Laramap</a></p>
+        
+        <div class="row">
+            <div class="col-md-6">
+                <p class="alert alert-warning">
+                   <strong><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 
+                    This app has been built by <a href="@amardarmanko">@amardarmanko</a>
+                    for learning purposes.</strong>
+                </p>
+            </div>
+        </div>
+
+        <p>
+            Feel free to help to improve the <a href="#">source code </a>.
+        </p>
+        
+        <hr>
+
+        <h2>What is Laramap?</h2>
+
+        <p>Laramap is the website by which laracarte was inspired :).</p>
+        <p>More info <a href="https://Laramap.com/p/about">here.</a></p>
+
+        <hr>
+
+        <h2>Which tools and service are used in laracarte ?</h2>
+
+        <p>Basically it'is built on laravel &amp; bootstrap.But there's a bunch of service used for email and other section. </p>
+
+        <ul>
+            <li>Laravel</li>
+            <li>Bootstrap</li>
+            <li>Amazon S3</li>
+            <li>Mandrill</li>
+            <li>Google Map</li>
+            <li>Gravatar</li>
+            <li>Antony Martin's Geolocation Package</li>
+            <li>Michel Fundation's Markdown Parser Package</li>
+            <li>Heroku</li>
+        </ul>
+
+    </div>      
+
+
+@stop
+
+
